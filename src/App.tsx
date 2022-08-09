e743ca17d6eb1a1b9bf426f3e2364fd872a93385
Christophe Deliquaire <christophe.deliquaire@gmail.com>
2022-08-09 22:53:47 +0200

    good

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Page from './Page.mdx'
+
+export default () => 
+<>
+    <header className='flex text-center'>
+        <h1 className='text-3xl font-bold'>
+            Hello world!
+        </h1>
+    </header>
+    <div id='content' className=''>
+
+        <Page />
+    </div>
+    <footer>
+
+    </footer>
+</>
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client';
-import Page from './Page.mdx'
+import App from './App'
 
 window.onload = function() {
-    // let Greet = () => <h1>Hello, world!</h1>
-    console.log('Well, that\'s kewll ! :)')
     const rootDiv = document.getElementById('root');
-    console.log(document);
-    console.log({rootDiv});
-    const root = ReactDOM.createRoot(
-        rootDiv
-    );
-    root.render((
-    <>
-        <header className='flex text-center'>    
-            <h1 className='text-3xl font-bold underline'>
-                Hello world!
-            </h1>
-        </header>
-        <div id='content' className=''>
-
-            <Page />
-        </div>
-        <footer>
-
-        </footer>
-    </>
-    ));
+    const root = ReactDOM.createRoot(rootDiv);
+    root.render((<App />));
 }
